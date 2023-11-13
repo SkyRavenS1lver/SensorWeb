a9076716c274b6b05e906cfe6dae1bbe9d930afc
SkyRavenS1lver <revandra.aryo.dwi.krisnandaru@mail.ugm.ac.id>
2023-11-13 10:14:08 +0700

    Update api

--- api.php
+++ api.php
@@ -3,41 +3,49 @@ include("dbconn.php");
 $request_method=$_SERVER["REQUEST_METHOD"];
 switch ($request_method) {
    case 'POST':
-    $data = json_decode(file_get_contents('php://input'), true);
+      $data = json_decode(file_get_contents('php://input'), true);
+      if (json_last_error() !== JSON_ERROR_NONE) {
+          // Handle JSON decoding error
+          echo "Invalid JSON data";
+          return;
+      }
+      
       $arrcheckpost = array(
-   "Light"=> '',
-   "PersentaseKelembapanTanah"=> '',
-   "AirQuality"=> '',
-   "RainDrop"=> '',
-   "H"=> '',
-   "T"=> '',
-   "Temperature"=> '',
-   "Pressure"=> '',
-   "ApproxAltitude"=> '');
-   $hitung = count(array_intersect_key($data, $arrcheckpost));
-   if($hitung == count($arrcheckpost)){
-    $postData = [
-        "Light"                       => "$data[Light]",
-         "PersentaseKelembapanTanah"   => "$data[PersentaseKelembapanTanah]",
-         "AirQuality"                  => "$data[AirQuality]",
-         "RainDrop"                    => "$data[RainDrop]",
-         "H"                           => "$data[H]",
-         "T"                           => "$data[T]",
-         "Temperature"                 => "$data[Temperature]",
-         "Pressure"                    => "$data[Pressure]",
-         "ApproxAltitude"              => "$data[ApproxAltitude]",
-         "TimeStamp"                   => date("Y-m-d,H:i:s"),
-    ];
+          "Light"=> '',
+          "PersentaseKelembapanTanah"=> '',
+          "AirQuality"=> '',
+          "RainDrop"=> '',
+          "H"=> '',
+          "T"=> '',
+          "Temperature"=> '',
+          "Pressure"=> '',
+          "ApproxAltitude"=> ''
+      );
 
-    $ref_tables = "sensor1";
-    $postRef = $database->getReference($ref_tables)->push($postData);
-    if($postRef){
-        echo "Data Saved";
-    }
-    else {
-        echo "Error";
-    }
+      $hitung = count(array_intersect_key($data, $arrcheckpost));
+      if($hitung == count($arrcheckpost)) {
+          $postData = [
+              "Light"                       => "$data[Light]",
+              "PersentaseKelembapanTanah"   => "$data[PersentaseKelembapanTanah]",
+              "AirQuality"                  => "$data[AirQuality]",
+              "RainDrop"                    => "$data[RainDrop]",
+              "H"                           => "$data[H]",
+              "T"                           => "$data[T]",
+              "Temperature"                 => "$data[Temperature]",
+              "Pressure"                    => "$data[Pressure]",
+              "ApproxAltitude"              => "$data[ApproxAltitude]",
+              "TimeStamp"                   => date("Y-m-d,H:i:s"),
+          ];
 
-    };
+          $ref_tables = "sensor1";
+          $postRef = $database->getReference($ref_tables)->push($postData);
+          if($postRef) {
+              echo "Data Saved";
+          } else {
+              echo "Error";
+          }
+      } else {
+          echo "Invalid data format";
+      }
 }
 ?>
